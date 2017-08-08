0261b65a8b068fc12c6c871b1aec219880ded7d0
elpinal <elpinal@users.noreply.github.com>
2017-08-08 14:35:53 +0900

    Introduce LLVM

--- main.go
+++ main.go
@@ -1,8 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"llvm.org/llvm/bindings/go/llvm"
+)
 
 func main() {
+	builder := llvm.NewBuilder()
+	mod := llvm.NewModule("gec")
+
+	main := llvm.FunctionType(llvm.Int32Type(), []llvm.Type{}, false)
+	llvm.AddFunction(mod, "main", main)
+	block := llvm.AddBasicBlock(mod.NamedFunction("main"), "entry")
+	builder.SetInsertPoint(block, block.FirstInstruction())
+
+	a := builder.CreateAlloca(llvm.Int32Type(), "a")
 	expr, err := parse([]byte("12"))
-	fmt.Println(expr, err)
+	if err != nil {
+		fmt.Fprintln(os.Stdout, err)
+	}
+	builder.CreateStore(llvm.ConstInt(llvm.Int32Type(), uint64(expr), false), a)
+
+	aVal := builder.CreateLoad(a, "a_val")
+	builder.CreateRet(aVal)
+
+	if err := llvm.VerifyModule(mod, llvm.ReturnStatusAction); err != nil {
+		fmt.Fprintln(os.Stdout, err)
+	}
+	//mod.Dump()
+
+	engine, err := llvm.NewExecutionEngine(mod)
+	if err != nil {
+		fmt.Fprintln(os.Stdout, err)
+	}
+
+	funcResult := engine.RunFunction(mod.NamedFunction("main"), []llvm.GenericValue{})
+	fmt.Println(funcResult.Int(false))
 }
